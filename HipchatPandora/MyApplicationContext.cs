5fd96e63876fe732ac3d9d96633aba0cf0852df3
Shaun Guth <sguth@practicefusion.com>
2014-08-15 13:31:25 -0700

    Initial commit

---
+++ HipchatPandora/MyApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using HipchatPandora.Properties;
+
+namespace HipchatPandora
+{
+    internal class MyApplicationContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+
+        public MyApplicationContext()
+        {
+            _trayIcon = new NotifyIcon
+                {
+                    Icon = Resources.AppIcon,
+                    ContextMenu = new ContextMenu(new[]
+                        {
+                            new MenuItem("Exit", Exit), 
+                        }),
+                    Visible = true
+                };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // hide this so the user doesn't have to mouse over it
+            _trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
